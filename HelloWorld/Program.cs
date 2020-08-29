7cce5577d3f7849870a4f2a7646b3b6c95ca10e7
dahlqvistida <55230391+dahlqvistida@users.noreply.github.com>
2020-08-29 18:34:10 +0200

    Age + Name
    Asks about your age and name

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,26 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
+
+            Console.WriteLine("Whats your name?");
+
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello! " + Name);
+            Console.WriteLine("You are " + age + " so you know.");
+
+            Console.WriteLine("Have a great day!");
+
+
+
+
+
+
         }
     }
 }
