1efecf6680f539e0df2eb277ba0159614e7da077
dahlqvistida <55230391+dahlqvistida@users.noreply.github.com>
2020-09-04 15:24:12 +0200

    Homework

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq.Expressions;
 
 namespace HelloWorld
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
@@ -40,6 +43,83 @@ namespace HelloWorld
                 Console.WriteLine("Oh.. sad to hear, still " + age + " years in the mind huh?");
                 Console.WriteLine("Say hi to the dead for me.");
             }
+
+            var iLikeMovies = true; 
+
+            while (iLikeMovies == true)
+            {
+
+                Console.WriteLine("Write the number of one of the movies");
+                Console.WriteLine("");
+                Console.WriteLine("(1) Hunger Games");
+                Console.WriteLine("(2) The Darkest Minds");
+                Console.WriteLine("(3) Tangled");
+                Console.WriteLine("(4) Dora");
+
+                int movie = Convert.ToInt32(Console.ReadLine());
+
+                switch(movie){
+
+                    case 1:
+
+                        Console.WriteLine("Thats an amazing movie!");
+                        Console.WriteLine("You have good taste!");
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("Thats an amazing movie!");
+                        Console.WriteLine("You have good taste!");
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("Thats a cute movie!");
+                        Console.WriteLine("You have good taste!");
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("Are you sure?");
+                        Console.WriteLine("That movie is awful, so is you raste.");
+
+                        iLikeMovies = false;
+                        break;
+                }
+
+            }
+
+            
+
+            for (int guesses = 5; guesses >= 0; guesses--){
+                
+                Console.WriteLine("What number between 0 and 10 am i thinking of?");
+
+                int guess = Convert.ToInt32(Console.ReadLine());
+                
+                int rightNumber = 4;
+
+                if (guess == rightNumber)
+                {
+                    Console.WriteLine("Yay you are right!");
+
+                    guesses = 0;
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Make a new guess, you have " + guesses +" guesses left");
+
+
+                }
+
+
+
+            }
+
+
+
 
             
   
