2d5f655e14871a4477d82c1763f2be7a0f5f9680
dahlqvistida <55230391+dahlqvistida@users.noreply.github.com>
2020-09-25 15:05:27 +0200

    h

---
+++ Homework_methods/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.SymbolStore;
+using System.Xml.Schema;
+
+namespace Homework_methods
+{
+    class Program
+    { 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            int[] addNumbers = { 5, 7, 10, 2, 8 };
+
+            string[] words = { "You", "Are", "Special" };
+
+            int[] listOfNumbers = { 7, 5, 9, 2, 4 };
+
+
+
+            Console.WriteLine(Add(addNumbers));
+
+            BackwardsWords(words);
+
+            Console.WriteLine("Biggest number:" + smallestVsBiggestNumber(listOfNumbers));
+
+        }
+        static int Add(int[] addNumbers)
+        {
+
+            int total = 1;
+
+            for (int i = 4; i >= 0; i--)
+            {
+               total = total + addNumbers[i];
+
+
+            }
+                
+            return total;
+        }
+
+        static void BackwardsWords(string [] words)
+        {
+            for (int i = 2; i >= 0; i--)
+            {
+                Console.WriteLine(words[i]);
+
+            }
+        }
+
+        static int smallestVsBiggestNumber(int[] listOfNumbers)
+        {
+            int smallestNumber;
+            int biggestNumber;
+
+            for (int i = 4; i >= 0; i--)
+            {
+                int j = i;
+
+                if (j >= listOfNumbers[0] && j >= listOfNumbers[1] && j >= listOfNumbers[2] && j >= listOfNumbers[3] && j >= listOfNumbers[4])
+                {
+
+                    biggestNumber = j;
+
+                    
+                }
+                else if (j <= listOfNumbers[0] && j <= listOfNumbers[1] && j <= listOfNumbers[2] && j <= listOfNumbers[3] && j <= listOfNumbers[4])
+                {
+
+                    smallestNumber = j;
+
+                }
+
+            }
+
+            return biggestNumber;
+            
+        }
+    }
+}
---
+++ Objekt Orientering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Objekt_Orientering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
